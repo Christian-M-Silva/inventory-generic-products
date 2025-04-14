2a908dd8868dce94075a0afa02a2e516e83b5b2f
Christian Silva <tininjacms@gmail.com>
2025-04-14 16:34:08 -0300

    feat(criaráFunçãoDeSomaAndBuild): Criará função de soma e configuração de build

--- index.ts
+++ index.ts
@@ -1,9 +1,47 @@
 import { Product } from "./interfaces";
 import { InventoryItem } from "./types";
 
-function sumStockProduct<T>(inventoryItens:InventoryItem<Product>[]): number {
-    const total: number = inventoryItens.reduce((accumulator: number, product: InventoryItem<Product>) => accumulator + product.quantity, 0);
+function sumStockProduct<T>(inventoryItens: InventoryItem<Product>[]): number {
+  const total: number = inventoryItens.reduce(
+    (accumulator: number, product: InventoryItem<Product>) =>
+      accumulator + product.quantity,
+    0
+  );
 
-    return total
+  return total;
 }
 
+const itens: InventoryItem<Product>[] = [
+  {
+    data: {
+      category: "clothing",
+      id: 1,
+      inStock: false,
+      name: "Camisa",
+      price: 1,
+    },
+    quantity: 1,
+  },
+  {
+    data: {
+      category: "electronics",
+      id: 2,
+      inStock: true,
+      name: "PC",
+      price: 1,
+    },
+    quantity: 1,
+  },
+  {
+    data: {
+      category: "food",
+      id: 3,
+      inStock: false,
+      name: "Hamburguer",
+      price: 1,
+    },
+    quantity: 1,
+  },
+];
+
+console.log(sumStockProduct(itens))
